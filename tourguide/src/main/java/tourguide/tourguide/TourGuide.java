e0603c23dcae22207fb01228f66e6bba44ae5b5e
Tan Jun Rong <tanjunrong@gmail.com>
2015-09-16 17:18:12 +0900

    [BUGFIX] Fixed overlay.disableClick() not working

--- tourguide/src/main/java/tourguide/tourguide/TourGuide.java
+++ tourguide/src/main/java/tourguide/tourguide/TourGuide.java
@@ -264,6 +264,9 @@ public class TourGuide {
             Log.w("tourguide", "Overlay's default OnClickListener is null, it will proceed to next tourguide when it is clicked");
             frameLayoutWithHole.setViewHole(mHighlightedView);
             frameLayoutWithHole.setSoundEffectsEnabled(false);
+            frameLayoutWithHole.setOnClickListener(new View.OnClickListener() {
+                @Override public void onClick(View v) {} // do nothing, disabled.
+            });
         }
     }
 
