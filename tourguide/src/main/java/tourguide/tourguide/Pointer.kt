cf0d231fe229437f9c2674904237bf9ef850e92d
Tan Jun Rong <tanjunrong@gmail.com>
2018-04-23 22:30:57 +0900

    clean up

--- tourguide/src/main/java/tourguide/tourguide/Pointer.kt
+++ tourguide/src/main/java/tourguide/tourguide/Pointer.kt
@@ -3,25 +3,4 @@ package tourguide.tourguide
 import android.graphics.Color
 import android.view.Gravity
 
-class Pointer @JvmOverloads constructor(var gravity: Int = Gravity.CENTER, var color: Int = Color.parseColor("#FFFFFF")) {
-
-//    /**
-//     * Set color
-//     * @param color
-//     * @return return Pointer instance for chaining purpose
-//     */
-//    fun setColor(_color: Int): Pointer {
-//        color = _color
-//        return this
-//    }
-//
-//    /**
-//     * Set gravity
-//     * @param gravity
-//     * @return return Pointer instance for chaining purpose
-//     */
-//    fun setGravity(_gravity: Int): Pointer {
-//        gravity = _gravity
-//        return this
-//    }
-}
+class Pointer @JvmOverloads constructor(var gravity: Int = Gravity.CENTER, var color: Int = Color.parseColor("#FFFFFF"))
